b202ab2d1353966fd449db2db50ef119e84199b6
RainNP <paramee.sakul@gmail.com>
2024-11-28 11:03:15 +0700

    map data

--- src/utils/getAllUsers.ts
+++ src/utils/getAllUsers.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { UserData } from "./submitForm";
+import { Data } from "../models/Data";
+import { DataType } from "../models/enums/DataType";
+import { skills } from "../data/graphData";
 
 export type UserResponse = {
   "id": string,
@@ -8,6 +11,12 @@ export type UserResponse = {
 
 export const getAllUsers = async () => {
   const data : UserResponse[] = (await axios.get('https://asia-southeast1-test-social-graph.cloudfunctions.net/webApi/api/all-users')).data;
-  console.log(data);
-  return data;
+  const users : Data[] = [];
+  data.forEach((user) => {
+    const index = parseInt(user.data.skill.match(/\d+/g)![0]);
+
+    users.push(new Data(user.id, "", DataType.User, skills[index]));
+  });
+  console.log(users);
+  return users;
 };
